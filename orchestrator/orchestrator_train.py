14278009e20a7ae3f345a422c7edbbe4bcce4436
CB2702 <79522375+CB2702@users.noreply.github.com>
2025-07-10 17:49:03 +0100

    staging_master_release

--- orchestrator/orchestrator_train.py
+++ orchestrator/orchestrator_train.py
@@ -7,17 +7,23 @@ from packages.rename.ml_ops.data_import.data_import import run_import_data
 from packages.rename.ml_ops.data_processing.data_processing import run_data_processing
 from packages.rename.ml_ops.training.train import run_model_training
 
-path = 'rename'
-print(f'Importing data from {path}.')
-df = run_import_data(path = path, name = 'mushrooms')
-print('Successfully imported data! See head sample below:')
-print(df.head())
+def main():
+    path = 'rename'
+    print(f'Importing data from {path}.')
+    df = run_import_data(path = path, name = 'mushrooms')
+    print('Successfully imported data! See head sample below:')
+    print(df.head())
 
-print('Processing data for feature engineering.')
-df = run_data_processing(df)
-print('Successfully run feature engineering for dataset! See head sample below:')
-print(df.head())
+    print('Processing data for feature engineering.')
+    df = run_data_processing(df)
+    print('Successfully run feature engineering for dataset! See head sample below:')
+    print(df.head())
 
-print('Running model training. This may take some time.')
-model = run_model_training(df, 0.3, 0.5)
-print('Successfully completed model training.')
+    print('Running model training. This may take some time.')
+    model = run_model_training(df, 0.3, 0.5)
+    print('Successfully completed model training.')
+
+    return None
+
+if __name__ == '__main__':
+    main()
